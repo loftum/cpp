ea11eaf6efc4719e63b933b627123da2aeb0bb6f
Hans Olav <loftum@gmail.com>
2018-11-19 21:09:33 +0100

    classes

--- src/classes.cpp
+++ src/classes.cpp
@@ -3,26 +3,33 @@ using namespace std;
 
 class Car 
 {
+    // Every member without accessor are private
     int speed;
     double orientation;
   public:
     // methods declared directly, are considered inline by the compiler
     void setSpeed(int speed){ this -> speed = speed; }
     int getSpeed(){ return speed; }
-    void setOrientation(double orientation);
+    double getOrientation();
+    // Members are NOT initialized (to 0) automatically.
+    // Uninitialized members have unpredictable values.
     Car(){speed = 0; orientation = 0;}
+    Car(int speed) {this -> speed = speed; orientation = 0;}
 };
 
 // methods declared outside of the class is not considered inline.
 // These will behave the same way, but not be inline optimized byt the compiler
-void Car::setOrientation(double orientation)
+double Car::getOrientation()
 {
-    this -> orientation = orientation;
+    return orientation;
 }
 
 int main()
 {
     Car car;
     car.setSpeed(42);
-    cout << car.getSpeed() << endl;
+    cout << car.getSpeed() << endl; // 42
+
+    auto car2 = Car(43);
+    cout << car2.getSpeed() << endl; // 43
 }
